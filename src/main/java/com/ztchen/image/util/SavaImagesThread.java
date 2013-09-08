f870ea8acfa40aed4b1339772de189f573459a65
ztchen89 <ztchen89@gmail.com>
2013-09-09 00:30:01 +0800

    增加图片下载线程

---
+++ src/main/java/com/ztchen/image/util/SavaImagesThread.java
@@ -0,0 +1,43 @@
+package com.ztchen.image.util;
+
+import java.io.File;
+
+public class SavaImagesThread extends Thread
+{
+	private File dir;
+	private String[] urls;
+	
+	public SavaImagesThread(File dir, String[] urls)
+	{
+		this.dir = dir;
+		this.urls = urls;
+	}
+	
+	@Override
+	public void run()
+	{
+		try
+		{
+			long start = System.currentTimeMillis();
+			
+			for (String url : urls)
+			{
+				int pos = url.lastIndexOf("/");
+				String imageName = url.substring(pos + 1);
+				
+				File file = new File(dir, imageName);
+				
+				GetJsonUtil.write2Local(url, file);
+			}
+			
+			long end = System.currentTimeMillis();
+			
+			System.out.println(Thread.currentThread().getName() +":" + (end - start));
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+
+	}
+}
